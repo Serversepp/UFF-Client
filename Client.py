2ab5b45bab15c3aec3578e4f9a2ec622c9a63a38
Sebastian <linuxusersese@gmail.com>
2022-02-14 12:12:40 +0100

    TLS

--- Client.py
+++ Client.py
@@ -1,8 +1,14 @@
 import socket
+import ssl
 import threading
 import tkinter
 import tkinter.scrolledtext
+import binascii
 from tkinter import simpledialog
+
+from Crypto.PublicKey import RSA
+from Crypto import Random
+
 class Client:
 
     def __init__(self, port, host): # host is ip of server
@@ -10,12 +16,13 @@ class Client:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         else:
             self.sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-            
-        self.sock.connect((host, port))
-        msg = tkinter.Tk()
-        msg.withdraw()
 
-        self.nick = simpledialog.askstring("Nickname", "PLease Enter your nickname", parent=msg)  #TODO add loop until Nick not already in use
+        ###TLS###
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock = ssl.wrap_socket(self.sock, keyfile=".\\Key\\MyKey.key", certfile=".\\Key\\MyCertificate.crt")
+        self.sock.connect((host, port))
+
+        if not self.logginprocedure(): exit(-2)
 
         self.running = True
         self.gui_build = False
@@ -25,6 +32,28 @@ class Client:
 
         gui_thread.start()
         recive_thread.start()
+
+    def logginprocedure(self):
+        self.random_generator = Random.new().read
+        msg = tkinter.Tk()
+        msg.withdraw()
+
+        self.configstring = self.sock.recv(1024).decode('utf-8')
+
+        if self.configstring[0] == 1:
+            print(" Server refused connection ")
+            exit(0)
+
+        if self.configstring[4] == '1':
+            pass # ADD PASSWD WITH RSA KEY EXCHANGE HERE
+
+        if self.configstring[1] == '1' and self.configstring[2:3] != "11":
+            pass # ADD AES KEY OVER RSA HERE
+
+
+        self.nick = simpledialog.askstring("Nickname", "PLease Enter your nickname",
+                                           parent=msg)  # TODO add loop until Nick not already in use
+        return 1
 
     def Gui_Loop(self):
         self.window = tkinter.Tk()
@@ -36,17 +65,18 @@ class Client:
 
         self.text_area = tkinter.scrolledtext.ScrolledText(self.window)
         self.text_area.pack(padx=20,pady=5)
-        self.text_area.config(state='disabled')
+        self.text_area.config(state='disabled', fg="lightgray", bg="black")
 
-        self.chat_label = tkinter.Label(self.window, text="Message", fg="lightgray", bg="black")
-        self.chat_label.config(font=("Arial", 12))
+        self.chat_label = tkinter.Label(self.window, text="Message")
+        self.chat_label.config(font=("Arial", 12), fg="lightgray", bg="black")
         self.chat_label.pack(padx=20, pady=5)
 
         self.input_area = tkinter.Text(self.window, height=5)
+        self.input_area.config( fg="lightgray", bg="black")
         self.input_area.pack(padx=20,pady=5)
 
         self.send_button = tkinter.Button(self.window, text="Send", command=self.write)
-        self.send_button.config(font=("Arial", 12))
+        self.send_button.config(font=("Arial", 12), fg="lightgray", bg="black")
         self.send_button.pack(padx=20, pady=5)
 
         self.gui_build = True
